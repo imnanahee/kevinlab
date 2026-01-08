733dcdcb881e8f07d091b0ac8999f7990472f359
imnanahee <nahee0824@gmail.com>
2026-01-08 15:29:59 +0900

    feat: 프로젝트 기초 세팅

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: false },
+  css: [
+    '~/assets/css/style.css',
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
